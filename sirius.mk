fcc2c8aaddd8fd4f797487780c8a54e15d8d7ba7
Max Weffers <rcstar6696@gmail.com>
2017-04-17 11:27:25 +0200

    sirius: Add missing mixer path file
    Change-Id: Iae04e11c7ec962018d58abda92147988a9e63be2

--- sirius.mk
+++ sirius.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
 
 # Thermal manager
 PRODUCT_COPY_FILES += \
