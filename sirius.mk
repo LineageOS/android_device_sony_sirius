3d59dd4c1dc017ed419586cac48ff2d8f1ba0cda
xkeita <keita_nya@live.jp>
2017-07-01 03:36:19 +0000

    sirius: Make audio configurations shinano common
    Change-Id: I15008b88f88231cfb31358094dfc772a18088785

--- sirius.mk
+++ sirius.mk
@@ -35,12 +35,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/d6502.sh:d6502.sh
 
-# Audio
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-   $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
-
 # Thermal manager
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
