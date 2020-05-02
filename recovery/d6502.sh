a77f7d82792ff1770da92b459144e750467cd4c8
Arian <arian.kulmer@web.de>
2020-05-02 19:53:16 +0200

    sirius: Fix support for D6502
    Change-Id: Id977e4b747fb3df8c06e808e465ffb8c97561b3f

--- recovery/d6502.sh
+++ recovery/d6502.sh
@@ -1,11 +1,16 @@
 #!/sbin/sh
 
-mkdir -p /lta-label
-mount -o ro /dev/block/platform/msm_sdcc.1/by-name/LTALabel /lta-label
-ls lta-label/*.html | grep -q d6502
+rm /tmp/d6502_log
+echo "d6502.sh started at $(date)" >> /tmp/d6502_log
+
+strings /dev/block/bootdevice/by-name/LTALabel | grep D6502
 if [ $? -eq 0 ]; then
-	mount /dev/block/platform/msm_sdcc.1/by-name/system /system
-	cp /system/etc/firmware/d6502/* /system/etc/firmware/
-	umount /system
+    echo "d6502 detected" >> /tmp/d6502_log
+    for i in $(ls /mnt/system/system/vendor/firmware/d6502/); do
+        mv /mnt/system/system/vendor/firmware/d6502/$i /mnt/system/system/vendor/firmware/
+    done
+    rm -rf /mnt/system/system/vendor/firmware/d6502/
+else
+    echo "nope, we are not d6502. removing d6502 firmware" >> /tmp/d6502_log
+    rm -rf /mnt/system/system/vendor/firmware/d6502/
 fi
-umount /lta-label
