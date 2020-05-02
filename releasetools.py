fb2506d505b3c8e8830dbae622db6a84d188fe37
Arian <arian.kulmer@web.de>
2020-05-02 19:53:16 +0200

    sirius: Fix support for D6502
    Change-Id: Id977e4b747fb3df8c06e808e465ffb8c97561b3f

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,7 @@
+#
 # Copyright (C) 2012 The Android Open Source Project
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +14,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-"""Custom OTA Package commands for sirius"""
-
-import os
-
-TARGET_DIR = os.getenv('OUT')
-TARGET_DEVICE = os.getenv('CM_BUILD')
-
-def FullOTA_InstallEnd(self):
-  self.output_zip.write(os.path.join(TARGET_DIR, "d6502.sh"), "d6502.sh")
-  self.script.AppendExtra('package_extract_file("d6502.sh", "/tmp/d6502.sh");')
-  self.script.AppendExtra('set_metadata("/tmp/d6502.sh", "uid", 0, "gid", 0, "mode", 0755);')
-  self.script.AppendExtra('run_program("/tmp/d6502.sh");')
+def FullOTA_InstallEnd(info):
+  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/bootdevice/by-name/system", "/mnt/system");');
+  info.script.AppendExtra('run_program("/tmp/install/bin/d6502.sh");')
+  info.script.AppendExtra('unmount("/mnt/system");');
+  return
